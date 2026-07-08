220f95163762c03ecca4529844cf9eb0f59c12b3
Steven Nguyen <153180925+snguyenwk@users.noreply.github.com>
2026-07-07 23:40:29 -0400

    Add: python-loop-unpacking - submission-0

---
+++ Python Coding Interviews/python-loop-unpacking/submission-0.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple
+
+# use unpacking in the loop to find the student with the highest score
+# return their name
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    best_score, best_student = 0, ""
+
+    for student, score in scores:
+        if score > best_score:
+            best_score, best_student = score, student
+    return best_student
+    
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
