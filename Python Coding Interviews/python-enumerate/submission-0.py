7c07b27d9a9bb3ae9cbf53885add686c9bcb4d75
Steven Nguyen <153180925+snguyenwk@users.noreply.github.com>
2026-07-08 00:22:15 -0400

    Add: python-enumerate - submission-0

---
+++ Python Coding Interviews/python-enumerate/submission-0.py
@@ -0,0 +1,30 @@
+from typing import List
+
+# use enumerate to return index of first occuernce of 7, return -1 otherwise
+def get_index_of_seven(nums: List[int]) -> int:
+    for i, num in enumerate(nums):
+        if nums[i] == 7:
+            return i
+    return -1
+
+# use enumerate to return distance between the occurences of number 7
+# maybe need i an j variables here?
+def get_dist_between_sevens(nums: List[int]) -> int:
+    first_seven_index = -1
+    for i, num in enumerate(nums):
+        if num == 7:
+            if first_seven_index == -1:
+                first_seven_index = i
+            else:
+                return i - first_seven_index
+            
+
+
+# do not modify below this line
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 8, 9]))
+print(get_index_of_seven([2, 4, 7, 5, 7, 8, 4, 2]))
+
+print(get_dist_between_sevens([1, 2, 7, 4, 5, 6, 7, 8, 9]))
+print(get_dist_between_sevens([2, 7, 7, 7, 8]))
+print(get_dist_between_sevens([7, 4, 8, 4, 2, 7]))
