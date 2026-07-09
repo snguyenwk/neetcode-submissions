b829d2fb3a059e12923a578654320a8da68534f3
Steven Nguyen <153180925+snguyenwk@users.noreply.github.com>
2026-07-09 17:19:13 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,11 @@
+# want to track both diff/values and indexes
+# return an index 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {} 
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in map:
+                return [map[diff], i]
+            map[num] = i
