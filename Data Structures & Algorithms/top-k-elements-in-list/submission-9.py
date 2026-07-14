cb6a9d147333a191bce8d4499e8a1003f9239556
Steven Nguyen <153180925+snguyenwk@users.noreply.github.com>
2026-07-14 14:36:47 -0400

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.py
@@ -0,0 +1,20 @@
+# we need a map and a freq sublist 
+# loop through nums and loop through the created count map
+# append num to freq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        
+        for num, count in count.items():
+            freq[count].append(num)
+        
+        res = []
+        for i in range(len(freq) -1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
