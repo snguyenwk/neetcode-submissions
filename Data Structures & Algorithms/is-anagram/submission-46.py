55e9106126fef137558e796c0cea6295f5bf8a0f
Steven Nguyen <153180925+snguyenwk@users.noreply.github.com>
2026-09-21 05:14:04 -0400

    Add: is-anagram - submission-46

---
+++ Data Structures & Algorithms/is-anagram/submission-46.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for val in count:
+            if val != 0:
+                return False
+        return True
+
+
+# given two strings, s and t
+# if they are anagrams, return true
+# else return false
+# iterate through two strings, store the value inside the string to track anagram potential
+# edge cases? empty strings? if length doesn't =, then no anagram
+
+class Solution:
+    def isAnagram(self, s: str, t:str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for val in count:
+            if val != 0:
+                return False
+        return True 
